fcca81480c773a48178c72ed1907757e05381c0e
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-06-06 15:22:57 +0200

    updated megalinter to put npm install updated index to put error

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,6 @@ ReactDOM.render(
   <React.StrictMode>
     <DashBoard />
     {/* <SignInUp /> */}
-  </React.StrictMode>,
+  </React.StrictMode>,aeeazeae:eae:d!!s!d!s:deaera
   document.getElementById("root")
 );
