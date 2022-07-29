4876cf436085489d915b9182b23bbfc9abef41f6
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2022-07-29 11:41:14 +0200

    add acceptance tests

--- src/acceptance_test/step_definitions/stepDefinition.steps.js
+++ src/acceptance_test/step_definitions/stepDefinition.steps.js
@@ -8,25 +8,41 @@ import DashBoard from "../../UI/WorkSpace/DashBoard";
 
 defineFeature(feature, (test)=> {
     test('User visits external links', ({ given, when, then }) => {
+        let variamos_link = "https://variamos.com/home/variamos-web/";
+        let doc_link = "https://github.com/variamosple/VariaMosPLE/wiki";
         given('the user is on the main page', () => {
             render(<DashBoard />);
-            expect(screen.getByRole('heading', {name: /project management/i})).toBeVisible()
-            // expect(1).toBe(1);
-            // return true;
-        });
-
-        when(/^the user clicks on the button (.*)$/, (arg0) => {
-            console.log(arg0)
             expect(screen.getByRole('listitem', {
                 name: /home/i
-            })).toBeDefined()
-            console.log('defined')
-            return true;
+            })).toBeVisible()
+
         });
 
-        then(/^the user should be sent to (.*)$/, (arg0) => {
-            console.log(arg0)
-            return true;
+        when(/^the user clicks on the button (.*)$/, (button) => {
+            //Check the button exists
+            if (button == "logo"){
+                expect(screen.getByRole('img')).toBeVisible()
+            }
+            else if (button == "home"){
+                expect(screen.getByRole('listitem', {name: /home/i})).toBeVisible()
+            }
+            else {
+                expect(screen.getByRole('listitem', {name: /view docs/i})).toBeVisible()
+            }
+        });
+
+        then(/^the user should be sent to (.*)$/, (website) => {
+            //Check the links
+            if (website == "variamos_home"){
+                // screen.debug(undefined, 30000)
+                // screen.getByRole('')
+                console.log(screen.getAllByRole('link', {name : ""})[0])
+            }
+            else {
+                expect(screen.getByRole('link', {name: "What is VariaMos?"})).toHaveAttribute(
+                    "href",
+                    doc_link)
+            }
         });
     });
 
